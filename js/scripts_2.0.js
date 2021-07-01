5bdf5093e9356fd151950af135c3460c8595ae6e
Sharkie <craig.sharkie@gmail.com>
2021-07-01 23:43:49 +1000

    Enhance UI
    - Added classes `filter filter-XXX` which act as anchors for Isotope to apply ... filtering
    - Moved Counters container further "up" in the DOM
    - Updated Counter number for Venues
    - Improved Counters container styling so it pops
    - Added Isotope library to attempt filtering
    - Added JavaScript to ensure all Venue are as tall as the tallest venue, and so the Grid is more predictable

--- js/scripts_2.0.js
+++ js/scripts_2.0.js
@@ -1,26 +1,49 @@
 ;(function () {
-  var launchpad = function () {
-    $(document).on('click', '.animate-box a', function (event) {
-      if ($(this).attr('href').indexOf("http") !== -1) {
-        event.preventDefault();
-        var url = $(this).attr('href'); 
-        window.open(url, '_blank');
-      }
-    });
-  };
+	var launchpad = function () {
+		$(document).on('click', '.animate-box a', function (event) {
+			if ($(this).attr('href').indexOf("http") !== -1) {
+				event.preventDefault();
+				var url = $(this).attr('href'); 
+				window.open(url, '_blank');
+			}
+		});
+	};
 
-  var shirthouse = function (state) {
-    $('.tshirt__switch:last-child').animate({ 'opacity' : state }, 'slow');
-    setTimeout(function () {
-      shirthouse(state ? 0 : 1);
-    }, 3000);
-  
-  };
+	var shirthouse = function (state) {
+		$('.tshirt__switch:last-child').animate({ 'opacity' : state }, 'slow');
+		setTimeout(function () {
+			shirthouse(state ? 0 : 1);
+		}, 3000);
+	
+	};
 
-	// Document on load.
+	var equality = function () {
+		var minimum = 0;
+		$('.filter').each(function () {
+			var venue = $(this);
+			console.log($(this).height())
+
+			if (venue.height() > minimum) {
+				minimum = venue.height();
+			}
+		}).height(minimum);
+
+		// alert(minimum)
+	};
+
+	var filter = function (filter) {
+		var selector = filter || 'filter-aus';
+
+		// $('.filter').isotope({
+		// 	itemSelector: selector
+		// });
+	};
+
+	// Document on load
 	$(function(){
 		launchpad();
 		shirthouse();
+		equality();
+		// filter();
 	});
-
 }());
