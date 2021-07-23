6145c6bc94a6d0e496a7efd2d3ea2b01c08b1031
Sharkie <craig.sharkie@gmail.com>
2021-07-23 22:16:08 +1000

    Housekeeping
    - Made the main JavaScript file more idiomatic
    - Updated Links for Light Reading MD

--- js/main.js
+++ js/main.js
@@ -2,26 +2,25 @@
 	'use strict';
 
 	// iPad and iPod detection	
-	var isiPad = function(){
+	var isiPad = function () {
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
-	var isiPhone = function(){
-	    return (
+	var isiPhone = function () {
+		return (
 			(navigator.platform.indexOf("iPhone") != -1) || 
 			(navigator.platform.indexOf("iPod") != -1)
-	    );
+		);
 	};
 
 	// Parallax
-	var parallax = function() {
-    $(window).stellar();
+	var parallax = function () {
+		$(window).stellar();
 	};
 
 	// Burger Menu
-	var burgerMenu = function() {
-
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
+	var burgerMenu = function () {
+		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
 
 			event.preventDefault();
 
@@ -30,128 +29,114 @@
 			} else {
 				$(this).addClass('active');	
 			}
-
 		});
-
 	};
 
-
 	// Page Nav
-	var clickMenu = function() {
+	var clickMenu = function () {
+		// $('#navbar a:not([class="external"])').click(function (event) {
+		$('#navbar a.internal').click(function (event) {
+			var section = $(this).data('nav-section');
+			var navbar = $('#navbar');
 
-		// $('#navbar a:not([class="external"])').click(function(event){
-    $('#navbar a.internal').click(function(event){
-			var section = $(this).data('nav-section'),
-				navbar = $('#navbar');
+			if ( $('[data-section="' + section + '"]').length ) {
+				$('html, body').animate({
+					scrollTop: $('[data-section="' + section + '"]').offset().top - 55
+				}, 500);
+			}
 
-				if ( $('[data-section="' + section + '"]').length ) {
-			    	$('html, body').animate({
-			        	scrollTop: $('[data-section="' + section + '"]').offset().top - 55
-			    	}, 500);
-			   }
+			if ( navbar.is(':visible')) {
+				navbar.removeClass('in');
+				navbar.attr('aria-expanded', 'false');
+				$('.js-fh5co-nav-toggle').removeClass('active');
+			}
 
-		    if ( navbar.is(':visible')) {
-		    	navbar.removeClass('in');
-		    	navbar.attr('aria-expanded', 'false');
-		    	$('.js-fh5co-nav-toggle').removeClass('active');
-		    }
-
-		    event.preventDefault();
-		    return false;
+			event.preventDefault();
+			return false;
 		});
-
-
 	};
 
 	// Reflect scrolling in navigation
-	var navActive = function(section) {
+	var navActive = function (section) {
+		console.log(section);
 
 		var $el = $('#navbar > ul');
 		$el.find('li').removeClass('active');
-		$el.each(function(){
+		$el.each(function () {
 			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
 		});
-
 	};
 
-	var navigationSection = function() {
-
+	var navigationSection = function () {
 		var $section = $('section[data-section]');
 		
-		$section.waypoint(function(direction) {
-		  	
-		  	if (direction === 'down') {
-		    	navActive($(this.element).data('section'));
-		  	}
+		$section.waypoint(function (direction) {
+			if (direction === 'down') {
+				navActive($(this.element).data('section'));
+			}
 		}, {
-	  		offset: '150px'
+			offset: '150px'
 		});
 
-		$section.waypoint(function(direction) {
-		  	if (direction === 'up') {
-		    	navActive($(this.element).data('section'));
-		  	}
+		$section.waypoint(function (direction) {
+			if (direction === 'up') {
+				navActive($(this.element).data('section'));
+			}
 		}, {
-		  	offset: function() { return -$(this.element).height() + 155; }
+			offset: function () { return -$(this.element).height() + 155; }
 		});
-
 	};
 
 	// Window Scroll
-	var windowScroll = function() {
+	var windowScroll = function () {
 		var lastScrollTop = 0;
 
-		$(window).scroll(function(event){
-
-		   	var header = $('#fh5co-header'),
-				scrlTop = $(this).scrollTop();
+		$(window).scroll(function (event) {
+			var header = $('#fh5co-header');
+			var	scrlTop = $(this).scrollTop();
 
 			if ( scrlTop > 500 && scrlTop <= 2000 ) {
 				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
 			} else if ( scrlTop <= 500) {
 				if ( header.hasClass('navbar-fixed-top') ) {
 					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
-					setTimeout(function(){
+					setTimeout(function () {
 						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
 					}, 100 );
 				}
-			} 
-			
+			}
 		});
 	};
 
-	var counter = function() {
+	var counter = function () {
 		$('.js-counter').countTo({
 			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
+				return value.toFixed(options.decimals);
+			},
 		});
 	};
 
-	var counterWayPoint = function() {
+	var counterWayPoint = function () {
 		if ($('#fh5co-counter-section').length > 0 ) {
-			$('#fh5co-counter-section').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
+			$('#fh5co-counter-section').waypoint( function ( direction ) {
+				if ( direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout( counter , 400);
 					$(this.element).addClass('animated');
 				}
-			} , { offset: '90%' } );
+			}, { offset: '90%' } );
 		}
 	};
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint( function ( direction ) {
+			if ( direction === 'down' && !$(this.element).hasClass('animated-fast')) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
+				setTimeout(function () {
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
 						setTimeout( function () {
 							var effect = el.data('animate-effect');
@@ -166,25 +151,22 @@
 							}
 
 							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
+						}, k * 50, 'easeInOutExpo' );
 					});
-					
 				}, 50);
-				
 			}
-
-		} , { offset: '85%' } );
+		}, { offset: '85%' } );
 	};
 
 	// Document on load.
 	$(function () {
-    if ($(window).stellar) {
-      parallax();
-      windowScroll();
-      navigationSection();
-      counterWayPoint();
-      contentWayPoint();
-    }
+		if ($(window).stellar) {
+			parallax();
+			windowScroll();
+			navigationSection();
+			counterWayPoint();
+			contentWayPoint();
+		}
 
 		burgerMenu();
 		clickMenu();
--- linksforlightreading.md
+++ linksforlightreading.md
@@ -2,7 +2,7 @@
 
 ##social
 
-https://www.instagram.com/mealsforhospomates/
+
 
 
 ##searches
@@ -11,6 +11,7 @@ https://www.google.com/search?q=venues+are+offering+free+meals
 https://www.google.com/search?q=free+meals+to+hospitality+workers
 https://www.google.com/search?q=free+hospo+meals
 
+
 ##archive
 
 https://bathurstonlinenews.com.au/these-sydney-venues-are-offering-free-meals-to-those-impacted-by-lockdown-concrete-playground/
@@ -18,4 +19,5 @@ https://bathurstonlinenews.com.au/these-sydney-venues-are-offering-free-meals-to
 
 ##broked
 
-https://www.ausvenueco.com.au/hospo-meals/
+https://www.ausvenueco.com.au/hospo-meals/ (2021, but over)
+https://www.instagram.com/mealsforhospomates/ (2020)
